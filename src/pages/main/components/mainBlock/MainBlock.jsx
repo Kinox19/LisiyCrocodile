1517d461a1e96ddfc5e29d79a4039e554eb8b021
Kinox19 <puchkouartem@gmail.com>
2023-04-25 12:00:02 +0300

    Работа с адаптивом, добавлена favicon, изменен index файл

--- src/pages/main/components/mainBlock/MainBlock.jsx
+++ src/pages/main/components/mainBlock/MainBlock.jsx
@@ -6,7 +6,7 @@ const MainBlock = () => {
     <div className={s.block}>
       <div className={s.textInfo}>
         <h1 className={s.heading}>ЛЫсый КРокодил</h1>
-        <p className={s.text}>Концерты Лысого Крокодила в Москве и Санкт-Петербурге – скоро!</p>
+        <p className={s.text}>Концерты Лысого Крокодила в&nbsp;Москве и&nbsp;Санкт-Петербурге&nbsp;&mdash; скоро!</p>
         <button className={s.button}>Подробнее</button>
       </div>
     </div>
