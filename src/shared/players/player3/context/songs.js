c5b91b15bcd1e78593cc090e550937c0fef4b71f
Kinox19 <puchkouartem@gmail.com>
2023-05-08 13:15:47 +0300

    Плеер мобилок теперь шикарен

--- src/shared/players/player3/context/songs.js
+++ src/shared/players/player3/context/songs.js
@@ -4,6 +4,6 @@ export const song_list = [
         artist: 'Лысый Крокодил',
         fileUrl: 'https://audio.jukehost.co.uk/cKDLHjsIxDN9sleIp51rEU6HrMBDAYBW',
         time: '2:16',
-        downloadUrl: 'https://drive.google.com/uc?export=download&id=13lrS2rrCNJupCS6SZisf3EPR2atg154U',
+        downloadUrl: 'https://drive.google.com/uc?export=download&id=1jvR9uMKDXVNv3tLAtLxd57keJNB1SVv4',
     },
   ]
--- src/shared/players/player4/context/songs.js
+++ src/shared/players/player4/context/songs.js
@@ -4,27 +4,27 @@ export const song_list = [
         artist: 'Лысый Крокодил',
         fileUrl: 'https://audio.jukehost.co.uk/rZd1Bkb9vfSJOnZhRXmHpVzqyO0zp0tw',
         time: '1:41',
-        downloadUrl: 'https://drive.google.com/uc?export=download&id=13lrS2rrCNJupCS6SZisf3EPR2atg154U',
+        downloadUrl: 'https://drive.google.com/uc?export=download&id=1vdb56zUAPwCZUzSskvKk-JiSrALEb6Mv',
     },
     {
         title: 'МАБИРАНСТВО',
         artist: 'Лысый Крокодил',
         fileUrl: 'https://audio.jukehost.co.uk/fjUXddBeEgTdA6lEFD8HonARtSQZN9WC',
         time: '1:50',
-        downloadUrl: 'https://drive.google.com/uc?export=download&id=13lrS2rrCNJupCS6SZisf3EPR2atg154U',
+        downloadUrl: 'https://drive.google.com/uc?export=download&id=1TomgZkIMFJGS6i_wvJe7-EnwmnCpDZEg',
     },
     {
         title: 'ЛЕНА, ПРИНЕСИ СТАКАНЫ!',
         artist: 'Лысый Крокодил',
         fileUrl: 'https://audio.jukehost.co.uk/3tLFeP9BGKfJLSE5HCx9bJtwRYFWaCqp',
         time: '1:44',
-        downloadUrl: 'https://drive.google.com/uc?export=download&id=13lrS2rrCNJupCS6SZisf3EPR2atg154U',
+        downloadUrl: 'https://drive.google.com/uc?export=download&id=1UUWjAVlgG8bmPZbl4fd4BuOwfg--X7wB',
     },
     {
         title: 'ЦВЕТОК ЗА 3.50',
         artist: 'Лысый Крокодил',
         fileUrl: 'https://audio.jukehost.co.uk/6FDejYP24fmaYxh8y6zwvg4pREGZR2An',
         time: "2:50",
-        downloadUrl: 'https://drive.google.com/uc?export=download&id=13lrS2rrCNJupCS6SZisf3EPR2atg154U',
+        downloadUrl: 'https://drive.google.com/uc?export=download&id=1P5T7-nl00rrYTIF7PHuQFtLhHY7DBKWj',
     }
   ]
